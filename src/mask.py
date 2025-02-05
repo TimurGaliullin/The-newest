de68f1e7d08ff0530a161f034bc96e4466344ea2
КЦ <timurgaliullin073@gmail.com>
2025-02-05 11:46:57 +0300

    main

---
+++ src/mask.py
@@ -0,0 +1,15 @@
+def get_mask_card_number(card_number: str):
+    a = card_number[:4]
+    b = card_number[4:6]
+    c = card_number[-4:]
+    '''Функция маскирует номер карты путём сложения частей её номера'''
+    full_card_number = a + ' ' + b + '** **** ' + c
+    return full_card_number
+
+def get_mask_account(account_number: str):
+    piece = account_number[-4:]
+    '''Функция возвращает значение при помощи f-строки'''
+    return f'** {piece}'
+
+
+
