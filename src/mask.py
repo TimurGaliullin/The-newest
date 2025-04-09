718a750ae82e981131db03e515e488222623f458
КЦ <timurgaliullin073@gmail.com>
2025-04-09 21:46:37 +0300

    1 part of tests

--- src/mask.py
+++ src/mask.py
@@ -1,4 +1,6 @@
 def get_mask_card_number(card_number: str) -> str:
+    if card_number == '':
+        return ''
     a = card_number[:4]
     b = card_number[4:6]
     c = card_number[-4:]
@@ -7,6 +9,8 @@ def get_mask_card_number(card_number: str) -> str:
     return full_card_number
 
 def get_mask_account(account_number: str) -> str:
+    if account_number == '':
+        return ''
     piece = account_number[-4:]
     '''Функция возвращает значение при помощи f-строки'''
     return f'** {piece}'
--- tests/tests_main.py
+++ tests/tests_main.py
@@ -5,3 +5,17 @@ from src.widget import date
 from src.processing import filter_by_state
 from src.processing import sort_by_date
 
+def test_get_mask_card_number():
+    assert get_mask_card_number('88005553535') == '8800 55** **** 3535'
+    assert get_mask_card_number('427981274832738') == '4279 81** **** 2738'
+    assert get_mask_card_number('hewhw2u37y32huu829') == 'hewh w2** **** u829'
+    assert get_mask_card_number('') == ''
+
+
+def test_get_mask_account():
+    assert get_mask_account('04580923') == '** 0923'
+    assert get_mask_account('438328903284083042') == '** 3042'
+    assert get_mask_account('qwy43gf78yr4370d08') == '** 0d08'
+    assert get_mask_account('') == ''
+
+
